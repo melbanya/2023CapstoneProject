390fcf3d553381c7f6c6a7092c67268307f2f845
박진희 <melbazzang@naver.com>
2023-12-27 11:27:25 +0900

    [Fix] 라우터 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,19 @@
+import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import App from '../App.vue'
+// import Page1 from './Page1.vue'
+// import Page2 from './Page2.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  // { path: '/page1', component: Page1 },
+  // { path: '/page2', component: Page2 }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
+
+createApp(App).use(router).mount('#app')
 
 export default router
